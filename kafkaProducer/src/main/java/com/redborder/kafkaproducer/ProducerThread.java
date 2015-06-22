8782a3503b511b360fa79d8d688fbae9c7bd5d18
Andrés Gomez <agomez@redborder.net>
2015-06-22 19:02:42 +0200

    Fixed locmse10 message.

--- kafkaProducer/src/main/java/com/redborder/kafkaproducer/ProducerThread.java
+++ kafkaProducer/src/main/java/com/redborder/kafkaproducer/ProducerThread.java
@@ -285,13 +285,11 @@ public class ProducerThread extends Thread {
                 "\"subscriptionName\":\"motus-MSE-Alpha80\",\"entity\":\"WIRELESS_CLIENTS\"," +
                 "\"deviceId\":\"" + client_mac + "\",\"lastSeen\":\"2015-02-24T08:45:48.154+0000\"," +
                 "\"ssid\":\"ssid-test\",\"band\":null,\"apMacAddress\":\""+ getAPmac() + "\"," +
-                "\"locationMapHierarchy\":\"Motus>Bottom>Bar\"," +
+                "\"locationMapHierarchy\":\"Motus>Bottom Bar>G\"," +
                 "\"locationCoordinate\":{\"x\":147.49353,\"y\":125.65644,\"z\":0.0," +
                 "\"unit\":\"FEET\"},\"confidenceFactor\":24.0,\"timestamp\":" + System.currentTimeMillis() + "}]}";
 
         String [] mse = new String[]{mse1, mse2};
-
-
 
         return new KeyedMessage<String, String>("rb_loc", client_mac, mse[randomX.nextInt(mse.length)]);
     }
